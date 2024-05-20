8a97031056c1aa9cb65c71ee840845b3154244f2
nikki0906 <160705654+nikki0906@users.noreply.github.com>
2024-05-20 11:00:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	const lowerStr = s1.toLowerCase();
+    const lowerSubStr = s2.toLowerCase();
+    
+    // Use the indexOf method to find the first occurrence
+    return lowerStr.indexOf(lowerSubStr);
 }
 
-// Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
